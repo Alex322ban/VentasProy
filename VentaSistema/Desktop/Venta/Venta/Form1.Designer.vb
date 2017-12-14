807725e06488b528139f3de13d679dd5b56de007
Alex322ban <31739245+Alex322ban@users.noreply.github.com>
2017-12-14 16:05:50 -0500

    Modelo

--- VentaSistema/Desktop/Venta/Venta/Form1.Designer.vb
+++ VentaSistema/Desktop/Venta/Venta/Form1.Designer.vb
@@ -84,7 +84,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(347, 188)
+        Me.ClientSize = New System.Drawing.Size(354, 180)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox2)
